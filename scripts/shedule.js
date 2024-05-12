f42b78c37b5508b64407703204d6dc6f2f8606b1
Rustdj <81041717+Rustdj@users.noreply.github.com>
2024-05-12 14:41:26 +0300

    create contacts page

--- scripts/shedule.js
+++ scripts/shedule.js
@@ -1,3 +1,37 @@
-// modal =================================================================================================
+// slider ========================================
+
+let sliderIndex = 1;
+
+showSlides(sliderIndex);
+
+function nextSlide() {
+    showSlides(sliderIndex += 1);
+}
+
+function previousSlide() {
+    showSlides(sliderIndex -= 1);
+} 
+function currentSlide(n) {
+    showSlides(sliderIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("");
+    if(n > slides.length) {
+        sliderIndex = 1
+    }
+    if (n < 1) {
+        sliderIndex = slides.length
+    }
+    
+    for (let slide of slides) {
+        slide.style.display = 'none';
+    }
+    slides[sliderIndex - 1].style.display = 'block';
+    
+}
+    
+
+
 
 
