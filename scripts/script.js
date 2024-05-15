f6bc5117f926bf1ce8b4a52c9f2a651656654b46
Rustdj <81041717+Rustdj@users.noreply.github.com>
2024-05-16 00:41:23 +0300

    create burger menu and form php

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,18 @@
 // slider
 // получение селекторов
 const slider = document.querySelector('.karusel__slider');
-const prevButton = document.querySelector('.arrow-left');
-const nextButton = document.querySelector('.arrow-right');
-const slides = Array.from(slider.querySelectorAll('img'));
-const sliderCount = slides.length;
-let sliderIndex = 0;
+      prevButton = document.querySelector('.arrow-left');
+      nextButton = document.querySelector('.arrow-right');
+      slides = Array.from(slider.querySelectorAll('img'));
+      sliderCount = slides.length;
+  let sliderIndex = 0;
+
+
 
 // устанавливаем обработчики событий к кнопкам
 prevButton.addEventListener('click', showPreveusSlide);
 nextButton.addEventListener('click', showNextSlide);
-slides.style.transform = `translateX(${slideOffset}px)`;
+// slides.style.transform = `translateX(${sliderIndex}px)`;
 
 // Функция для показа слайда
 function showPreveusSlide() {
--- scripts/shedule.js
+++ scripts/shedule.js
@@ -1,35 +1,3 @@
-// slider ========================================
-
-// let sliderIndex = 1;
-
-// showSlides(sliderIndex);
-
-// function nextSlide() {
-//     showSlides(sliderIndex += 1);
-// }
-
-// function previousSlide() {
-//     showSlides(sliderIndex -= 1);
-// } 
-// function currentSlide(n) {
-//     showSlides(sliderIndex = n);
-// }
-
-// function showSlides(n) {
-//     let slides = document.getElementsByClassName("");
-//     if(n > slides.length) {
-//         sliderIndex = 1
-//     }
-//     if (n < 1) {
-//         sliderIndex = slides.length
-//     }
-    
-//     for (let slide of slides) {
-//         slide.style.display = 'none';
-//     }
-//     slides[sliderIndex - 1].style.display = 'block';
-    
-// }
 
 // form
 
